044f76be1cd121945ac775791b44cc5c3140ac32
Sardar Ali <sardaralikhamosh@gmail.com>
2026-02-25 01:04:04 +0500

    zst technical services llc
    updated the site to make it more dynemic and professional

--- index.php
+++ index.php
@@ -10,6 +10,13 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <!--favicon-->
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+    <!--favicon-->
+    <meta name="p:domain_verify" content="b5ca7078d5fc16c9a41b4836a83832a9"/>
 </head>
 <body>
     <!-- Header/Navigation -->
